966972f1b4e5ea9321016c34ccc571b88169124c
Rupali Shewale <rupalishewale2005@gmail.com>
2025-12-11 21:50:03 +0530

    modified path

--- Sketchpy_test/draw.py
+++ Sketchpy_test/draw.py
@@ -1,4 +1,10 @@
 from sketchpy import canvas
+import os
 
-d = canvas.sketch_from_image("Doraemon.jpg")
-d.draw()
+# Check if file exists
+img_path = r"D:\Programs\python\internship\Sketchpy_test\Doraemon.jpg"
+if not os.path.exists(img_path):
+    print("Image not found!")
+else:
+    d = canvas.sketch_from_image(img_path)
+    d.draw()
